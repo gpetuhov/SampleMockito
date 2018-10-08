b58a5fb8e46ce8746ce8ac89f73eff3db4ec8492
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-10-08 11:52:36 +0300

    Refactor PrefsUtils to Kotlin

---
+++ app/src/main/java/com/gpetuhov/samplemockito/utils/PrefsUtils.kt
@@ -0,0 +1,31 @@
+package com.gpetuhov.samplemockito.utils
+
+import android.content.SharedPreferences
+
+import com.gpetuhov.samplemockito.SampleMockitoApp
+
+import javax.inject.Inject
+
+// Helper class for reading and writing SharedPreferences
+// This class will be tested with Mockito
+class PrefsUtils {
+
+    companion object {
+        private const val TEXT_KEY = "text_key"
+        private const val DEFAULT_TEXT = "default_text"
+    }
+
+    init {
+        SampleMockitoApp.getDataComponent().inject(this)
+    }
+
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
+
+    var text: String?
+        get() = sharedPreferences.getString(TEXT_KEY, DEFAULT_TEXT)
+        set(value) = sharedPreferences
+                .edit()
+                .putString(TEXT_KEY, value)
+                .apply()
+}
